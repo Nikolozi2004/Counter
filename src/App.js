f15abd43afde8cad16a1feff23bae3110ba1c007
Nikolozi2004 <undeadmaster08@gmail.com>
2022-08-13 20:40:29 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
+import {Fragment, useState} from 'react';
 import './App.css';
 
 function App() {
+  let [counter, setCounter] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Fragment>
+      <div className='wrapper'>
+    <button onClick={() => setCounter(counter + 1)} className='mainButton'>Up</button>
+    <div className='counterContainer'>{counter}</div>
+    <button onClick={() => setCounter(counter - 1)} className='mainButton'>Down</button>
+    <button onClick={() => setCounter(counter = 0)} className='resetCounter'>Reset</button>
     </div>
+    <div className='optionsWrapper'>
+    <button onClick={() => setCounter(counter + 10)} className='button10'>+10</button>
+    <button onClick={() => setCounter(counter - 10)} className='button10 toLeft'>-10</button>
+    <button onClick={() => setCounter(counter + 50)} className='button10'>+50</button>
+    <button onClick={() => setCounter(counter - 50)} className='button10 toLeft'>-50</button>
+    <button onClick={() => setCounter(counter + 100)} className='button10'>+100</button>
+    <button onClick={() => setCounter(counter - 100)} className='button10 toLeft'>-100</button>
+    </div>
+    </Fragment>
   );
 }
 
